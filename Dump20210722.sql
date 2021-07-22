b66669d209c5ff0cb9cd33e684e580ce500aacb3
David <david.alx.glz@gmail.com>
2021-07-22 01:36:41 -0500

    bd

---
+++ Dump20210722.sql
@@ -0,0 +1,203 @@
+CREATE DATABASE  IF NOT EXISTS `dwi_api` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dwi_api`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dwi_api
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `car`
+--
+
+DROP TABLE IF EXISTS `car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car` (
+  `idcar` int NOT NULL AUTO_INCREMENT,
+  `idusuarios` int NOT NULL,
+  PRIMARY KEY (`idcar`),
+  KEY `fk_car_usuarios1_idx` (`idusuarios`),
+  CONSTRAINT `fk_car_usuarios1` FOREIGN KEY (`idusuarios`) REFERENCES `usuarios` (`idusuarios`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car`
+--
+
+LOCK TABLES `car` WRITE;
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `idproducts` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `idSeller` int NOT NULL,
+  `img` varchar(45) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`idproducts`),
+  KEY `fk_products_usuarios1_idx` (`idSeller`),
+  CONSTRAINT `fk_products_usuarios1` FOREIGN KEY (`idSeller`) REFERENCES `usuarios` (`idusuarios`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Playera Roja',1,'playeraroja',199.99),(2,'Playera gris',1,'playeragris',123.45);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products_has_car`
+--
+
+DROP TABLE IF EXISTS `products_has_car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products_has_car` (
+  `products_idproducts` int NOT NULL,
+  `car_idcar` int NOT NULL,
+  `cantidad` int DEFAULT NULL,
+  PRIMARY KEY (`products_idproducts`,`car_idcar`),
+  KEY `fk_products_has_car_car1_idx` (`car_idcar`),
+  KEY `fk_products_has_car_products1_idx` (`products_idproducts`),
+  CONSTRAINT `fk_products_has_car_car1` FOREIGN KEY (`car_idcar`) REFERENCES `car` (`idcar`),
+  CONSTRAINT `fk_products_has_car_products1` FOREIGN KEY (`products_idproducts`) REFERENCES `products` (`idproducts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products_has_car`
+--
+
+LOCK TABLES `products_has_car` WRITE;
+/*!40000 ALTER TABLE `products_has_car` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products_has_car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rating`
+--
+
+DROP TABLE IF EXISTS `rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rating` (
+  `idrating` int NOT NULL AUTO_INCREMENT,
+  `usuarios_idusuarios` int NOT NULL,
+  `products_idproducts` int NOT NULL,
+  `valueRating` decimal(2,1) DEFAULT NULL,
+  PRIMARY KEY (`idrating`),
+  KEY `fk_rating_usuarios1_idx` (`usuarios_idusuarios`),
+  KEY `fk_rating_products1_idx` (`products_idproducts`),
+  CONSTRAINT `fk_rating_products1` FOREIGN KEY (`products_idproducts`) REFERENCES `products` (`idproducts`),
+  CONSTRAINT `fk_rating_usuarios1` FOREIGN KEY (`usuarios_idusuarios`) REFERENCES `usuarios` (`idusuarios`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rating`
+--
+
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+INSERT INTO `rating` VALUES (1,2,1,4.0),(2,1,1,2.0),(3,2,2,4.0),(4,1,2,4.0);
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `idroles` int NOT NULL AUTO_INCREMENT,
+  `rol` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idroles`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin'),(2,'buyer'),(3,'seller');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idusuarios` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(300) DEFAULT NULL,
+  `password` varchar(300) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `roles_idroles` int NOT NULL,
+  PRIMARY KEY (`idusuarios`),
+  KEY `fk_usuarios_roles_idx` (`roles_idroles`),
+  CONSTRAINT `fk_usuarios_roles` FOREIGN KEY (`roles_idroles`) REFERENCES `roles` (`idroles`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'david.gonzalez','$2a$10$Xt.YoGIrsakklNdARTorR.PMN6VGyhdSLGBAFiKMkRrRgOXE/wbGm','David Alejandro','González González',3),(2,'jona.duron','$2a$10$8Grq3TwYPz8e.iuMz8/LruajbVX/eH/688nlgNeCQCiVUEQ3MOZly','Jonathan Alberto','Durito',2);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'dwi_api'
+--
+
+--
+-- Dumping routines for database 'dwi_api'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-22  1:35:13
